ba69f6ade7e74b7dcba92f0709fc8b483e0739a4
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-09-27 14:14:01 +0200

    Switch to ureq

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,5 +1,5 @@
-use serde::Deserialize;
 use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+use serde::Deserialize;
 
 /// https://url.spec.whatwg.org/#fragment-percent-encode-set
 const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
@@ -18,7 +18,7 @@ struct Update {
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct Chat {
-    pub id: i64
+    pub id: i64,
 }
 
 #[derive(Deserialize, Clone, Debug)]
@@ -36,37 +36,43 @@ pub struct Telegram {
 
 impl Telegram {
     pub fn from(token: &str) -> Self {
-        Telegram{token: token.to_string(), last_update: None}
+        Telegram {
+            token: token.to_string(),
+            last_update: None,
+        }
     }
     fn base_url(&self) -> String {
         format!("https://api.telegram.org/bot{}/", &self.token)
     }
-    pub fn read_messages(&mut self) -> Result<Vec<Message>, reqwest::Error> {
+    pub fn read_messages(&mut self) -> Result<Vec<Message>, ureq::Error> {
         println!("{:?}", self.last_update);
-        let updates: TgResult = reqwest::get(format!(
-            "{}getUpdates?timeout=10{}",
-            self.base_url(),
-            {
+        let updates: TgResult =
+            ureq::get(&format!("{}getUpdates?timeout=10{}", self.base_url(), {
                 if let Some(o) = &self.last_update {
                     format!("&offset={}", o)
                 } else {
                     "".to_string()
                 }
-            }
-        ).as_str())?.json()?;
+            }))
+            .call()?
+            .into_json()?;
         if updates.result.len() == 0 {
             return Ok(Vec::new());
         }
         self.last_update = Some(updates.result[&updates.result.len() - 1].update_id + 1);
         Ok(updates.result.iter().map(|x| x.message.clone()).collect())
     }
-    pub fn send_message(&self, message: &Message) -> Result<(), reqwest::Error> {
-        reqwest::get(format!(
-            "{}sendMessage?chat_id={}&text={}",
-            self.base_url(),
-            message.chat.id,
-            utf8_percent_encode(message.text.clone().unwrap().as_str(), FRAGMENT)).as_str()
-        )?;
+    pub fn send_message(&self, message: &Message) -> Result<(), ureq::Error> {
+        ureq::get(
+            format!(
+                "{}sendMessage?chat_id={}&text={}",
+                self.base_url(),
+                message.chat.id,
+                utf8_percent_encode(message.text.clone().unwrap().as_str(), FRAGMENT)
+            )
+            .as_str(),
+        )
+        .call()?;
         Ok(())
     }
 }
